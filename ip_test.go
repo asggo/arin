1611cf646b8109bcbde813f899082808041344ad
Stephen Haywood <stephen@averagesecurityguy.info>
2018-01-30 21:12:18 -0500

    Additional tests.

--- ip_test.go
+++ ip_test.go
@@ -5,15 +5,46 @@ import (
 )
 
 
-func TestGetWhoisIP(t *testing.T) {
+func TestNewWhoisIP(t *testing.T) {
+    name := "AT-88-Z"
+    handle := "NET-52-32-0-0-1"
+    parentName := "NET52"
+    parentHandle := "NET-52-0-0-0-0"
     orgName := "Amazon Technologies Inc."
-    ip := GetWhoisIP("52.52.52.0")
+    orgHandle := "AT-88-Z"
+    cidr := "52.32.0.0/11"
+
+    ip := NewWhoisIP("52.52.52.0")
 
     if ip == nil {
         t.Error("Expected an IP record but got nil.")
     }
 
+    if ip.Name != name {
+        t.Error("Expected", name, "got", ip.Name)
+    }
+
+    if ip.Handle != handle {
+        t.Error("Expected", handle, "got", ip.Handle)
+    }
+
+    if ip.Parent.Name != parentName {
+        t.Error("Expected", parentName, "got", ip.Parent.Name)
+    }
+
+    if ip.Parent.Handle != parentHandle {
+        t.Error("Expected", parentHandle, "got", ip.Parent.Handle)
+    }
+
     if ip.Organization.Name != orgName {
-        t.Error("Expected", orgName, "got", ip.Parent.Name)
+        t.Error("Expected", orgName, "got", ip.Organization.Name)
+    }
+
+    if ip.Organization.Handle != orgHandle {
+        t.Error("Expected", orgHandle, "got", ip.Organization.Handle)
+    }
+
+    if ip.NetBlocks[0].Cidr() != cidr {
+        t.Error("Expected", cidr, "got", ip.NetBlocks[0].Cidr())
     }
 }
