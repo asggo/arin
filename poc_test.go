4813117fcaacc634ecb6cfd0fdc78c8b771c2a7e
AverageSecurityGuy <stephen@averagesecurityguy.info>
2018-01-31 13:01:37 -0500

    Add support for point of contacts.

---
+++ poc_test.go
@@ -0,0 +1,31 @@
+package arin
+
+import (
+    "testing"
+    "fmt"
+)
+
+var c = &Contact{
+    Name: "Registration Services Department",
+    Handle: "ARIN-HOSTMASTER",
+    Company: "American Registry for Internet Numbers",
+    Address: "3635 Concorde Parkway\nSuite 200",
+    City: "Chantilly",
+    StateProv: "VA",
+    PostalCode: "20121",
+    Country: "US",
+    Registered: "2003-04-30",
+    Updated: "2011-07-21",
+    Phone: "+1-703-227-0660 (Office)",
+    Email: "hostmaster@arin.net",
+}
+
+
+
+func TestNewContact(t *testing.T) {
+    contact := NewContact("ARIN-HOSTMASTER")
+
+    if !contact.Equal(c) {
+        t.Errorf("Expected\n%s\ngot\n%s\n", c, contact)
+    }
+}
