9dd47cf53cc26b710aa844df66b82629fba35633
AverageSecurityGuy <stephen@averagesecurityguy.info>
2018-01-31 20:01:47 -0500

    Formatted address string.

--- poc.go
+++ poc.go
@@ -9,7 +9,8 @@ type Contact struct {
 	Name       string
 	Handle     string
 	Company    string
-	Address    string
+	Address1   string
+	Address2   string
 	City       string
 	StateProv  string
 	PostalCode string
@@ -20,20 +21,30 @@ type Contact struct {
 	Email      string
 }
 
+func (c *Contact) addressString() string {
+	var s []string
+
+	s = append(s, c.Address1)
+
+	if c.Address2 != "" {
+		s = append(s, c.Address2)
+	}
+
+	s = append(s, fmt.Sprintf("%s, %s  %s", c.City, c.StateProv, c.PostalCode))
+	s = append(s, c.Country)
+
+	return strings.Join(s, "\n")
+}
+
 func (c *Contact) String() string {
 	var s []string
 
-	s = append(s, fmt.Sprintf("Name: %s", c.Name))
-	s = append(s, fmt.Sprintf("Handle: %s", c.Handle))
-	s = append(s, fmt.Sprintf("Company: %s", c.Company))
-	s = append(s, fmt.Sprintf("Address: %s", c.Address))
-	s = append(s, fmt.Sprintf("City: %s", c.City))
-	s = append(s, fmt.Sprintf("StateProv: %s", c.StateProv))
-	s = append(s, fmt.Sprintf("PostalCode: %s", c.PostalCode))
+	s = append(s, fmt.Sprintf("%s (%s)", c.Name, c.Handle))
+	s = append(s, c.addressString())
+	s = append(s, c.Phone)
+	s = append(s, c.Email)
 	s = append(s, fmt.Sprintf("Registered: %s", c.Registered))
 	s = append(s, fmt.Sprintf("Updated: %s", c.Updated))
-	s = append(s, fmt.Sprintf("Phone: %s", c.Phone))
-	s = append(s, fmt.Sprintf("Email: %s", c.Email))
 
 	return strings.Join(s, "\n")
 }
@@ -42,10 +53,12 @@ func (c *Contact) Equal(c2 *Contact) bool {
 	return c.Name == c2.Name &&
 		c.Handle == c2.Handle &&
 		c.Company == c2.Company &&
-		c.Address == c2.Address &&
+		c.Address1 == c2.Address1 &&
+		c.Address2 == c2.Address2 &&
 		c.City == c2.City &&
 		c.StateProv == c2.StateProv &&
 		c.PostalCode == c2.PostalCode &&
+		c.Country == c2.Country &&
 		c.Registered == c2.Registered &&
 		c.Updated == c2.Updated &&
 		c.Phone == c2.Phone &&
@@ -61,11 +74,20 @@ func NewContact(handle string) *Contact {
 	}
 
 	rec := parseRecord(record)
+	addr := strings.Split(rec["Address"], "\n")
 
 	c.Name = rec["Name"]
 	c.Handle = rec["Handle"]
 	c.Company = rec["Company"]
-	c.Address = rec["Address"]
+
+	switch len(addr) {
+	case 2:
+		c.Address1 = addr[0]
+		c.Address2 = addr[1]
+	default:
+		c.Address1 = addr[0]
+	}
+
 	c.City = rec["City"]
 	c.StateProv = rec["StateProv"]
 	c.PostalCode = rec["PostalCode"]
